657d812d8c90135deaea523904a377055da3de29
jdtatsch <jdtatsch@gmail.com>
2018-04-06 20:22:42 -0300

    dummy chg

--- _deploy.sh
+++ _deploy.sh
@@ -8,8 +8,8 @@ set -e
 git config --global user.email "jdtatsch@gmail.com"
 git config --global user.name "jdtatsch"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git bookdown-demo-output
-cd bookdown-demo-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git bookdown-demo-out
+cd bookdown-demo-out
 cp -r ../_book/* ./
 git add --all *
 git commit -m "Update the bookdown-demo" || true
