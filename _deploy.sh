6fbee09696ed21ade22240a769758f94325d06af
jdtatsch <jdtatsch@gmail.com>
2018-04-06 19:29:42 -0300

    change vars to jdtatsch and bookdown-demo

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "jdtatsch@gmail.com"
+git config --global user.name "jdtatsch"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git bookdown-demo-output
+cd bookdown-demo-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Update the bookdown-demo" || true
 git push -q origin gh-pages
